f36b759b77702934b6d293ddbcac8943a4c985bf
KusokEnota <enimausy@gmail.com>
2023-04-24 09:31:19 +0300

    reshenie dz

--- 2.py
+++ 2.py
@@ -1,1 +1,5 @@
 # Задание 2. Пользователь вводит время в секундах
+secund = int(input("Введите время в сек.: "))
+chas = secund // 3600
+minuta = secund // 60
+print(f"время в формате ч:м:с - {chas:.1f}:{minuta:.1f}:{secund: }")
